0e39382d04525d88366b11459159ea53df57c7ec
Jasmine Ramirez <j.ramirez.14th@gmail.com>
2019-04-15 15:37:30 -0500

    added a gitignore file

---
+++ DataAnalysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+bar_locations = pd.read_csv("bar_locations.csv")
+party_nyc = pd.read_csv("party_in_nyc.csv")
+test_parties = pd.read_csv("test_parties.csv")
+train_parties = pd.read_csv("train_parties.csv")
+
+# print(bar_locations[:5])
--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-
 import kaggle as kg
 kg.api.authenticate()
 kg.api.dataset_download_files(dataset='somesnm/partynyc', path='.', unzip=True, force=True)
-
 
 
 
